658993cbae410eb727f818b5ebcdb1ea32c0a5e2
RonnieRBG <johnatan.rodriguezg@gmail.com>
2025-08-15 20:00:07 -0400

    agreado javadoc

--- ConversorTemperatura.java
+++ ConversorTemperatura.java
@@ -4,9 +4,14 @@ import java.util.Scanner;
 
 public class ConversorTemperatura {
 
+	
+
 	public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+/**
+ * @param gradosCelsius: ingresa tempratura en grados celsius
+ * @return regresa en grados farenheit
+ */
         System.out.print("Ingresa Grados C°: ");
         double gradosCelsius = sc.nextDouble();
 
